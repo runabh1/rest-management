362025c7f3a945a1111c36ae543515eef31769e1
runabh1 <runabh1@github.com>
2026-06-18 22:13:04 +0530

    cc

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -232,29 +232,7 @@ function promiseGet(sql, params = []) {
   });
 }
 
-// Promise wrapper for database operations
-function dbRun(sql, params = []) {
-  return new Promise((resolve, reject) => {
-    db.run(sql, params, function(err) {
-      if (err) reject(err);
-      else resolve({ lastID: this.lastID, changes: this.changes });
-    });
-  });
-}
-
-function dbGet(sql, params = []) {
-  return new Promise((resolve, reject) => {
-    db.get(sql, params, (err, row) => {
-      if (err) reject(err);
-      else resolve(row);
-    });
-  });
-}
-
-function dbAll(sql, params = []) {
-  return new Promise((resolve, reject) => {
-
-// Promise wrapper for database operations
+// Promise wrappers for general database operations
 function dbRun(sql, params = []) {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function(err) {
